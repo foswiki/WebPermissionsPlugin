303f866f9075f928718c43322880191578223a50
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/WebPermissionsPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/build.pl
+++ lib/Foswiki/Plugins/build.pl
@@ -5,19 +5,19 @@
 package WebPermissionsPluginBuild;
 
 BEGIN {
-  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
-    unshift @INC, $pc;
-  }
+    foreach my $pc ( split( /:/, $ENV{FOSWIKI_LIBS} ) ) {
+        unshift @INC, $pc;
+    }
 }
 
 use Foswiki::Contrib::Build;
 
-@WebPermissionsPluginBuild::ISA = ( "Foswiki::Contrib::Build" );
+@WebPermissionsPluginBuild::ISA = ("Foswiki::Contrib::Build");
 
 sub new {
     my $class = shift;
-    return bless( $class->SUPER::new( "WebPermissionsPlugin" ), $class );
+    return bless( $class->SUPER::new("WebPermissionsPlugin"), $class );
 }
 
 $build = new WebPermissionsPluginBuild();
-$build->build($build->{target});
+$build->build( $build->{target} );
